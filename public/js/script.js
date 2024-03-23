5dde694e1e8ddbd922304cc357dcf25faf85a473
codeyumm <pvashi006@gmail.com>
2024-03-23 18:26:23 -0400

    Added view for word detail

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const seeMoreSoundButton = document.getElementById('see-more-sound-button');
     const seeMoreSpellingButton = document.getElementById('see-more-spelling-button');
+    const seeMoreAdjectiveButton = document.getElementById('see-more-adjective-button');
 
     seeMoreSoundButton.addEventListener('click', function () {
         const hiddenSoundWords = document.querySelectorAll('.similar-sound-container .word-container.hidden');
@@ -19,7 +20,16 @@ document.addEventListener('DOMContentLoaded', function () {
             wordContainer.classList.remove('hidden');
         });
 
-        
         seeMoreSpellingButton.style.display = 'none';
+    });
+
+    seeMoreAdjectiveButton.addEventListener('click', function () {
+        const hiddenAdjectiveWords = document.querySelectorAll('.similar-adjective-container .word-container.hidden');
+
+        hiddenAdjectiveWords.forEach(function (wordContainer) {
+            wordContainer.classList.remove('hidden');
+        });
+
+        seeMoreAdjectiveButton.style.display = 'none';
     });
 });
