5ebfbadc77cb0e566702c82e889760cbe7c08e49
codeyumm <pvashi006@gmail.com>
2024-03-23 17:27:50 -0400

    added api

--- modules/merriam-webster/api.js
+++ modules/merriam-webster/api.js
@@ -21,6 +21,25 @@ async function getDetailsOfWord( word ){
 
 }
 
+async function getDetailFromThesaurus( word ){
+        // link of api
+        const url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.MERRIAM_WEBSTER_THESAURUS_API_KEY}`;
+
+        // set header for api request
+        let options = {
+            method: 'get',
+            header: {
+                "Content-Type": "application/json"
+            }
+    
+        }
+    
+        let response = await fetch(url, options);
+    
+        return response.json();
+}
+
 module.exports = {
-    getDetailsOfWord
+    getDetailsOfWord,
+    getDetailFromThesaurus
 };
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -50,6 +50,8 @@ router.route('/details').get( async(request, response) => {
 
     wordDetails = await merriamWebster.getDetailsOfWord( word );
 
+    wordDetailFromThesaurus = await merriamWebster.getDetailFromThesaurus( word );
+
     // array to store all possible meaning of word
     // const definations = [];
     let defination = wordDetails[0].shortdef;
@@ -58,12 +60,63 @@ router.route('/details').get( async(request, response) => {
     //   definations.push(defination.shortdef)
     // });
 
+    var synonyms = [];
+    var antonyms = [];
 
     if( defination == null){
       defination = "No defination found for this word."
-    }
+      
+    } else {
 
-    response.status(200).render('word-defination', { word: word, defination: defination });
+
+    if( wordDetailFromThesaurus[0].meta != undefined  ){
+            // synonyms
+        
+            if( wordDetailFromThesaurus[0].meta.syns.length != 0 ){
+
+              synonyms = [...(wordDetailFromThesaurus[0].meta.syns)[0]];
+
+              // check for second element in syns if exist push to array otherwise push empty string
+              if (wordDetailFromThesaurus[0].meta.syns[1]) {
+
+                  synonyms.push(...wordDetailFromThesaurus[0].meta.syns[1]);
+
+              } else {
+
+                  synonyms.push("");
+
+              }
+            }
+
+
+
+          
+            if( wordDetailFromThesaurus[0].meta.ants.length != 0 ){
+              antonyms = [...(wordDetailFromThesaurus[0].meta.ants)[0]];
+
+              // check for second element in syns if exist push to array otherwise push empty string
+              if (wordDetailFromThesaurus[0].meta.ants[1]) {
+
+                antonyms.push(...wordDetailFromThesaurus[0].meta.ants[1]);
+
+              } else {
+
+                antonyms.push("");
+
+              }
+            }
+
+  } else {
+    synonyms = ["No synonyms found"];
+    antonyms = ["No antonyms found"];
+  }
+
+}
+
+   
+
+
+    response.status(200).render('word-defination', { word: word, defination: defination, synonyms: synonyms, antonyms: antonyms });
 })
 
 
